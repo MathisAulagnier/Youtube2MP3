4dae82ed3d547da732027cb67c67af240d561b6c
Mathis Aulagnier <mathis.aulagnier@telecomnancy.eu>
2023-11-02 21:51:07 +0100

    first commit

---
+++ 1.py
@@ -0,0 +1,22 @@
+from pytube import YouTube
+import os
+from pathlib import Path
+
+
+def youtube2mp3 (url,outdir):
+    # url input from user
+    yt = YouTube(url)
+
+    ##@ Extract audio with 160kbps quality from video
+    video = yt.streams.filter(abr='160kbps').last()
+
+    ##@ Downloadthe file
+    out_file = video.download(output_path=outdir)
+    base, ext = os.path.splitext(out_file)
+    new_file = Path(f'{base}.mp3')
+    os.rename(out_file, new_file)
+    ##@ Check success of download
+    if new_file.exists():
+        print(f'{yt.title} has been successfully downloaded.')
+    else:
+        print(f'ERROR: {yt.title}could not be downloaded!')
