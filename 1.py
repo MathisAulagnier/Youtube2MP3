dbdc4b0f07f2fcea439c223cd6860a82a0285fb0
Mathis Aulagnier <mathis.aulagnier@telecomnancy.eu>
2023-11-04 11:52:33 +0100

    Final

--- 1.py
+++ 1.py
@@ -1,22 +1,34 @@
 from pytube import YouTube
-import os
-from pathlib import Path
+import argparse
 
 
-def youtube2mp3 (url,path,ext):
+def youtube2mp3 (url="https://www.youtube.com/watch?v=Cp9pk-FkE6E", ext = 'M'):
     yt = YouTube(url)
     title = yt.title
-    if ext == '.mp3':
+    print('Loading...')
+    if ext == 'music'or ext == 'm' or ext == 'M' or ext == 'Music':
         son = yt.streams.filter(only_audio=True)
-        print(son)
         yt.streams.get_by_itag(son[-1].itag).download()
-    else:
+        print('Success !')
+    elif ext == 'video' or ext == 'v' or ext == 'V' or ext == 'Video' :
         video = yt.streams.filter(file_extension='mp4')
-        print(video)
         yt.streams.get_by_itag(video[1].itag).download()
+        print('Success !')
+    else:
+        print('Erreur, format non reconnu')
 
 if __name__ == '__main__':
-    url = 'https://www.youtube.com/watch?v=Cp9pk-FkE6E'
-    path = '.'
-    ext = '.mp3'
-    youtube2mp3(url,path,ext)
+    parser = argparse.ArgumentParser()
+    
+    parser.set_defaults(url="https://www.youtube.com/watch?v=Cp9pk-FkE6E")
+
+    parser.add_argument("--url", type=str, help="URL de la vidéo Youtube à télécharger")
+    parser.add_argument("--f", type=str, help="Format à télécharger : M pour musique, V pour vidéo")
+    
+    parser.set_defaults(url="https://www.youtube.com/watch?v=Cp9pk-FkE6E", f="M")
+
+    args = parser.parse_args()
+    print(args.url)
+    print(args.f)
+    
+    youtube2mp3(args.url,args.f)
